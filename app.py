18cd754b70151bf3adb61e586611545413da1765
Hayespace <ben.hayes@hotmail.com>
2023-10-19 11:51:43 +0000

    set debug to false

--- app.py
+++ app.py
@@ -355,4 +355,4 @@ def test_get_videos_route():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
